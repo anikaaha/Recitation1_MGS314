fe5320032df846531f9b1af796b9fccddd71d7f5
anikaaha <54906932+anikaaha@users.noreply.github.com>
2021-09-08 20:13:51 -0400

    Add files via upload

---
+++ recitation 1.py
@@ -0,0 +1,23 @@
+print (6+1)
+print (10/2)
+print (6%2) # because no remainder of 6/2
+print (7%2)
+print(2**2) # 2^2
+random= 12.67
+print (random)
+print (random+0.33)
+strings = 'whats up?'
+print (strings)
+print (type(strings))
+print (type(random))
+
+points_obtained= 45
+total_points= 50
+percentage = (points_obtained/total_points)*100
+print(percentage)
+
+x='Anika Ahad'
+print (x[0])
+print(x[5])
+print (x[-1])
+
